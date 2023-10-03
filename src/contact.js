4d41e5147de93d9b7b68d285b5166820de33b6b4
hkaur02 <hkaur@udel.edu>
2023-10-03 00:58:34 -0400

    second attempt at form

--- src/contact.js
+++ src/contact.js
@@ -49,62 +49,62 @@ function validateForm(){
         document.getElementById(id).textContent = '';
     });
 
-    ["fname", "lname", "email", "message", "phoneNumber"].forEach(id => {
+    ["user_fname", "user_lname", "user_email", "user_message", "user_phone"].forEach(id => {
         document.getElementById(id).classList.remove('invalid');
     });
 
-    let firstName = document.getElementById('fname').value;
-    let lastName = document.getElementById('lname').value;
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phoneNumber').value;
-    let message = document.getElementById('message').value;
+    let user_fname = document.getElementById('user_fname').value;
+    let user_lname = document.getElementById('user_lname').value;
+    let user_email = document.getElementById('user_email').value;
+    let user_phone = document.getElementById('user_phone').value;
+    let user_message = document.getElementById('user_message').value;
 
-    if(!firstName){
+    if(user_fname){
         isValid = false;
         let firstNameVal = document.getElementById("fnameValidation");
         firstNameVal.textContent = "First name is required";
         firstNameVal.style.color = "red";
-        document.getElementById('fname').classList.add('invalid');
+        document.getElementById('user_fname').classList.add('invalid');
     }
 
-    if(!lastName){
+    if(user_lname){
         isValid = false;
-        let lastName = document.getElementById("lnameValidation");
-        lastName.textContent = "Last name is required";
-        lastName.style.color = "red";
-        document.getElementById('lname').classList.add('invalid');
+        let lastNameVal = document.getElementById("lnameValidation");
+        lastNameVal.textContent = "Last name is required";
+        lastNameVal.style.color = "red";
+        document.getElementById('user_lname').classList.add('invalid');
     }
 
-    if(!email){
+    if(!user_email){
         isValid = false;
-        let email = document.getElementById("emailValidation");
-        email.textContent = "Email is required";
-        email.style.color = "red";
-        document.getElementById('email').classList.add('invalid');
+        let user_email = document.getElementById("emailValidation");
+        user_email.textContent = "Email is required";
+        user_email.style.color = "red";
+        document.getElementById('user_email').classList.add('invalid');
     }else{
         if(!emailPattern.test(email)){
             isValid = false;
             let emailPattern = document.getElementById("emailValidation");
             emailPattern.textContent = "Please enter an email address in the form: xyz@domain.<upper-level domain>";
             emailPattern.style.color = "red";
-            document.getElementById('email').classList.add('invalid');
+            document.getElementById('user_email').classList.add('invalid');
         }
     }
 
-    if(!message){
+    if(!user_message){
         isValid = false;
-        let message = document.getElementById("messageValidation");
-        message.textContent = "A message is required";
-        message.style.color = "red";
-        document.getElementById('message').classList.add('invalid');
+        let user_message = document.getElementById("messageValidation");
+        user_message.textContent = "A message is required";
+        user_message.style.color = "red";
+        document.getElementById('user_message').classList.add('invalid');
     }
 
-    if(!phoneNumber && !phoneNumberPattern.test(phone)){
+    if(!user_phone && !phoneNumberPattern.test(phone)){
         isValid = false;
         let phonePattern = document.getElementById("phoneValidation");
         phonePattern.textContent = "Please enter a phone number in the form: 123-456-7890";
         phonePattern.style.color = "red";
-        document.getElementById('phoneNumber').classList.add('invalid');
+        document.getElementById('user_phone').classList.add('invalid');
     }
 
     return isValid;
